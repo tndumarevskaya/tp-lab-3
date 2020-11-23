783620461be0c3f716b4b61cf71b09bb755a5964
tndumarevskaya <63363543+tndumarevskaya@users.noreply.github.com>
2020-11-23 17:32:05 +0300

    fix task1

--- src/task1.cpp
+++ src/task1.cpp
@@ -3,12 +3,13 @@
 double calcDelta()//расчет зазора между землей и веревкой
 {
 	Circle earth(6378.1);
-	Circle rope(earth.getRadius() + 1);
-	return rope.getArea() - earth.getArea();
+	Circle rope(0);
+    rope.setFerence(earth.getFerence() + 1);
+	return rope.getRadius() - earth.getRadius();
 };
 double calcCost() // расчет стоимости бассейна
 {
 	Circle pool(3);
 	Circle road(pool.getRadius() + 1);
-	return (pool.getArea() - road.getArea()) * 1000 * 1 +  road.getFerence() * 2000;
+	return (road.getArea() - pool.getArea()) * 1000 * 1 +  road.getFerence() * 2000;
 };
